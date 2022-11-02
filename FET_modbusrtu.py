0dbd9413e0950549d2f1d9aebabfcacb3828805a
TacoJimmy <t5669016@ntut.org.tw>
2022-11-02 16:53:25 +0800

    Update FET_modbusrtu.py

--- FET_modbusrtu.py
+++ FET_modbusrtu.py
@@ -54,7 +54,7 @@ def read_3p3w_meter(PORT,ID,loop):
         MainPW_meter[3] =  round(pw_cur[2] * 0.01,1)
         MainPW_meter[4] =  round(pw_power[0] * 0.01,1) 
         MainPW_meter[5] =  round(pw_pf[0]*0.001,1)
-        MainPW_meter[6] =  round((pw_consum[1]* 65536 + pw_consum[0] * 65535)*0.1,1)
+        MainPW_meter[6] =  round((pw_consum[0]* 65536 + pw_consum[1] * 65535)*0.1,1)
         MainPW_meter[7] = 1 
         master.close()
         time.sleep(0.5)
