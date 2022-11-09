eb2a355ebd7e78cde957a5e2c8b1d134b0d19df8
TacoJimmy <t5669016@ntut.org.tw>
2022-11-09 17:53:43 +0800

    Update FET_modbustcp.py

--- FET_modbustcp.py
+++ FET_modbustcp.py
@@ -248,9 +248,9 @@ def power_count():
     with open('static/data/PowerLoop01.json', 'w') as g:
         json.dump(power_kwh01, g)
     g.close
-    print (power_kwh01["power03_kwh"])
-    print (power_kwh01["power04_kwh"])
-    print (power_kwh01["power05_kwh"])
+    #print (power_kwh01["power03_kwh"])
+    #print (power_kwh01["power04_kwh"])
+    #print (power_kwh01["power05_kwh"])
     
     powermeter02 = (getPowerLoop02('192.168.1.11',502,380,0.9))
     
@@ -263,9 +263,9 @@ def power_count():
     with open('static/data/PowerLoop02.json', 'w') as g:
         json.dump(power_kwh02, g)
     g.close
-    print (power_kwh02["power06_kwh"])
-    print (power_kwh02["power07_kwh"])
-    print (power_kwh02["power08_kwh"])
+    #print (power_kwh02["power06_kwh"])
+    #print (power_kwh02["power07_kwh"])
+    #print (power_kwh02["power08_kwh"])
     
 if __name__ == '__main__':
     
